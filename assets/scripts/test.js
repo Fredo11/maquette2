222be15a6d8a117e956a49320f5b87c517f76e01
Fredo11 <tmboy11@bbox.fr>
2018-05-07 16:35:30 +0200

    essai pour voir

---
+++ assets/scripts/test.js
@@ -0,0 +1,14 @@
+alert('Bonjour');
+var message1 = 'Une autre chaine de caracteres';
+
+var message2 = "C'est une chaine de caracteres ...";
+
+
+// maintenant, on les affiche
+
+alert(message1);
+
+alert(message2);
+var age = prompt("Quel âge avez-vous ? (en années)"); // on demande l'age
+
+alert("Vous avez " + age + " ans");          // on affiche la phrase
